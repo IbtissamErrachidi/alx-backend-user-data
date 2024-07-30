385c54bed561b43a37850c5ffaff674537d60a0a
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-30 11:58:25 +0100

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ Returns an obfuscated log message
 """
 import re
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
-
