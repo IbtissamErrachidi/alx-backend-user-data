a1bba4e4614d8644ef0c1f50c92e54cdc7881edd
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-08-10 14:41:32 +0000

    Update the @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,15 +32,22 @@ elif AUTH_TYPE == "session_auth":
 @app.before_request
 def before_request() -> str:
     """Handle the Authentication"""
-    if not auth or not auth.require_auth(
-        request.path,
-        ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
-    ):
-        return
-    if auth.authorization_header(request) is None:
-        return abort(401)
-    if auth.current_user(request) is None:
-        return abort(403)
+    if auth is None:
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+        if auth.require_auth(request.path, excluded):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
