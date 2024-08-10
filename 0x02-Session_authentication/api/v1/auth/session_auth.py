00a642ab7fea704e6ab94a4b70b15ec7b5460150
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-08-10 14:19:18 +0000

    session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,17 @@
 """Session Authentication"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication class"""
+    user_id_by_session_id = {}  # Attribut de classe
 
-    pass
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user with id user_id  """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
